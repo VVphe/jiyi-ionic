0b46aa7f3c44106482a7183d1c6d08f0e94b4b40
Vv <crazyphe@outlook.com>
2019-02-03 14:46:32 +0800

    社区init

---
+++ src/app/community/community.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommunityComponent } from './community/community.component';
+
+@NgModule({
+  declarations: [CommunityComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommunityModule { }
